3d6bcbb04b00e73da3558ab68bb2b38fdd2f9e4d
huzzii <huzzii466@gmail.com>
2022-07-13 18:26:29 +0530

    doubly Linked List insert first and display function added

--- linked-list/doubleLinkedList/index.php
+++ linked-list/doubleLinkedList/index.php
@@ -12,10 +12,40 @@ class Node{
 }
 
 class DoublyLinkedList{
-    public $headNode;
-    public $tailNode;
-    public $count;
+    public $headNode = null;
+    public $tailNode =  null;
+    public $count = 0;
 
+    public function display(){
+        $current = $this->headNode;
+        while ($current !== null) {
+            echo $current->data . "->";
+            $current = $current->next;
+        }
+        echo "null\n";
+    }
+
+    public function insertAtFirst($data){
+        $newNode = new Node($data);
+
+        if ($this->headNode == null) {
+            $this->headNode = $newNode;
+            $this->tailNode = $newNode;
+        } else {
+            $prevHeadNode = $this->headNode;
+            $this->headNode = $newNode;
+            $this->headNode->next = $prevHeadNode;
+            $prevHeadNode->prev = $newNode;
+        }   
+    }
 }
+
+$dl = new DoublyLinkedList;
+$dl->insertAtFirst(3);
+$dl->insertAtFirst(2);
+$dl->insertAtFirst(1);
+$dl->insertAtFirst(0);
+$dl->insertAtFirst(-1);
+$dl->display();
 
 ?>
